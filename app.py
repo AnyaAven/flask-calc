060dc9e0abc18413f82e337e4ba9c00ee99a1431
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-24 15:32:13 -0400

    Started writing first route response and imported operations.py functions. WIP.

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 # Put your app in here.
+from flask import Flask
+import operations
+
+app = Flask(__name__)
+
+
+@app.get("/add")
+def add_numbers():
+    """ Add a and b and return result. """
+
+    a = request.args["a"]
+    b = request.args["b"]
+
+    add(a, b)
+
+# get the values of a and b
+# use add function imported from operations to add
+# add result to body of html string and return
